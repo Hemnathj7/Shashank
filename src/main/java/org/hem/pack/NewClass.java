a5f805bb4cd803b5f1333de1b6af36d290738796
Hemnath <Hemnath@Hemnath-FF>
2024-03-27 14:51:16 +0530

    abcd

--- src/main/java/org/hem/pack/NewClass.java
+++ src/main/java/org/hem/pack/NewClass.java
@@ -5,6 +5,9 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("sunday");
+System.out.println("monday");
+System.out.println("tuesday");
+System.out.println("wednesday");
 	}
 
 }
